2ad5df94e4447881332ee6fc0970473a0cb9525f
Samarth <221b325@juetguna.in>
2024-11-30 02:35:12 +0530

    change db url

--- db/connection.js
+++ db/connection.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const connection = mysql.createConnection({
-    url: process.env.DATABASE_URL
+    url: process.env.MYSQL_URL
 });
 
 module.exports = connection.promise();
